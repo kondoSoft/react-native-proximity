3d83799d91d6e1222fa68adafc3edb0b2d760ba8
Carlos Gonzalez <carlos@kondosoft.com>
2019-07-02 11:04:33 -0500

    fixed import for android X

--- android/src/main/java/com/estimote/react/RNEstimoteProximityModule.java
+++ android/src/main/java/com/estimote/react/RNEstimoteProximityModule.java
@@ -4,7 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
